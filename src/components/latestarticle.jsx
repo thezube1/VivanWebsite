ab56c4033e207d90afa449339b627d049842179d
Zubin <zubin.hydrie@hotmail.com>
2020-06-11 18:59:29 -0700

    Added text highlight effect

--- src/components/latestarticle.jsx
+++ src/components/latestarticle.jsx
@@ -1,93 +1,87 @@
 import React, { Component } from "react";
+import { motion } from "framer-motion";
 
-class LatestArticle extends Component {
-  state = {};
-  changeColorEnter = (e) => {
-    e.target.style.backgroundColor = "rgba(252, 186, 3, 0.65)";
-  };
-  changeColorLeave = (e) => {
-    e.target.style.backgroundColor = "rgba(252, 186, 3, 0)";
-  };
-
-  render() {
-    return (
+const LatestArticle = () => {
+  return (
+    <div
+      id="latestArticle"
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr",
+        gridTemplateRows: "1fr 140px 1fr",
+        gridTemplateAreas: `"." "desc" "latestTopic"`,
+      }}
+    >
       <div
-        id="latestArticle"
         style={{
-          display: "grid",
-          gridTemplateColumns: "1fr",
-          gridTemplateRows: "1fr 140px 1fr",
-          gridTemplateAreas: `"." "desc" "latestTopic"`,
+          height: 20,
+          width: 20,
+          backgroundColor: "black",
+          display: "flex",
+          alignItems: "center",
+          gridArea: "desc",
         }}
       >
         <div
           style={{
-            height: 20,
-            width: 20,
-            backgroundColor: "black",
-            display: "flex",
-            alignItems: "center",
-            gridArea: "desc",
+            fontSize: 30,
+            fontFamily: "Unna",
+            fontStyle: "italic",
+            fontWeight: "bold",
+            position: "relative",
+            left: "150%",
           }}
         >
-          <div
-            style={{
-              fontSize: 30,
-              fontFamily: "Unna",
-              fontStyle: "italic",
-              fontWeight: "bold",
-              position: "relative",
-              left: "150%",
-            }}
-          >
-            latest
-          </div>
+          latest
         </div>
-        <a
-          href="https://www.livemint.com/news/india/hope-and-anxiety-among-liberalization-s-children-11577803916064.html"
-          target="_blank"
+      </div>
+      <a
+        href="https://www.livemint.com/news/india/hope-and-anxiety-among-liberalization-s-children-11577803916064.html"
+        target="_blank"
+        style={{
+          textDecoration: "none",
+          gridArea: "latestTopic",
+          color: "black",
+        }}
+      >
+        <motion.span
           style={{
-            textDecoration: "none",
-            gridArea: "latestTopic",
-            color: "black",
+            fontFamily: "Unna",
+            fontWeight: "bold",
+            fontSize: 18,
+            position: "relative",
+            bottom: 40,
+          }}
+          whileHover={{
+            backgroundImage:
+              "linear-gradient(to right, rgba(252, 186, 3, .65) 100%, white 0%)",
+          }}
+          transition={{ duration: 0.5 }}
+        >
+          The secret behind <br /> millennial support
+          <br />
+          for India's Modi
+        </motion.span>
+        <div
+          style={{
+            display: "grid",
+            gridAutoColumns: "470px",
+            gridColumn: "main1",
+            fontFamily: "Roboto, sans-serif",
+            fontWeight: 100,
+            lineHeight: 1.36,
           }}
         >
-          <div
-            style={{
-              fontFamily: "Unna",
-              fontWeight: "bold",
-              fontSize: 18,
-              position: "relative",
-              bottom: 40,
-              backgroundColor: "rgba(252, 186, 3, 0)",
-            }}
-            onMouseOver={this.changeColorEnter}
-            onMouseLeave={this.changeColorLeave}
-          >
-            Hope and anxiety among liberalization’s children
-          </div>
-          <div
-            style={{
-              display: "grid",
-              gridAutoColumns: "470px",
-              gridColumn: "main1",
-              fontFamily: "Roboto, sans-serif",
-              fontWeight: 100,
-              lineHeight: 1.36,
-            }}
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            explicabo quisquam architecto minima, necessitatibus dolores odit
-            quod modi excepturi ducimus suscipit, ullam eaque sunt. Perspiciatis
-            laborum explicabo officia vel laudantium reiciendis dolore,
-            doloribus, dignissimos vero possimus saepe veniam facilis odit.
-            Voluptatem, eius laborum ea amet dolore voluptatum fuga corrupti
-            error.
-          </div>
-        </a>
-      </div>
-    );
-  }
-}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          explicabo quisquam architecto minima, necessitatibus dolores odit quod
+          modi excepturi ducimus suscipit, ullam eaque sunt. Perspiciatis
+          laborum explicabo officia vel laudantium reiciendis dolore, doloribus,
+          dignissimos vero possimus saepe veniam facilis odit. Voluptatem, eius
+          laborum ea amet dolore voluptatum fuga corrupti error.
+        </div>
+      </a>
+    </div>
+  );
+};
 
 export default LatestArticle;
